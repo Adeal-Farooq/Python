8acbdef3890e7bbb4398517f3ff39ef117f9e122
Adeal-Farooq <loneadeal@gmail.com>
2026-08-10 03:21:34 +0530

    while loop in python

---
+++ 12_while_loop.py
@@ -0,0 +1,16 @@
+#other programming language 
+#while loop , for loop , do while loop
+#but in python 
+#while loop , and for loop only
+
+
+#initialization
+#while condition:
+  #code
+  #increment/decrement
+
+number = int(input("Enter a number:"))
+i = 1
+while i <=10:
+  print(number, "x", i, "=" ,number*i)
+  i+=1
